48c967a9d30eaafaef3585b2d5f06bf039550ead
DevCrumbs <mail@devcrumbs.com>
2023-03-31 16:17:06 +0200

    types in test_appellation

--- lib/single_wine_tile.dart
+++ lib/single_wine_tile.dart
@@ -7,138 +7,157 @@ class SingleWineTile extends StatelessWidget {
   double fontSizeDesc = 17.0;
   double fontSizeSymbol = 17.0;
   final Map<String, dynamic> result;
+  
+  // static variable to store the height of the SingleWineTile widget
+  static double tileHeight = 0;
 
   SingleWineTile({required this.result});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: EdgeInsets.only(top: 10),
-        child: ListTile(
-        title: Text(result['FullName'],style: TextStyle(fontSize: fontSizeName,)),
-        subtitle: Column(
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                Expanded(
-                  child: Text(
-                      '${result['Pairing']}',
-                      style: TextStyle(fontSize: fontSizeDesc),
-                      softWrap: true,
+    return SizedBox(
+      child: LayoutBuilder(
+        builder: (BuildContext context, BoxConstraints constraints) {
+          // calculate height of the SingleWineTile widget based on its content
+          double contentHeight = (fontSizeName + 40 + ((fontSizeSymbol * 2) + 2)) ;
+          double rsSectionWidth = constraints.maxWidth * 0.6;
+          double qpSectionWidth = constraints.maxWidth * 0.6;
+          double rsBarWidth = result['RS'] / 100 * rsSectionWidth;
+          double qpBarWidth = result['QP'] / 100 * qpSectionWidth;
+          contentHeight += rsSectionWidth > 0 ? 26 : 0;
+          contentHeight += qpSectionWidth > 0 ? 26 : 0;
+
+          // update tileHeight with the actual calculated height
+          tileHeight = contentHeight;  
+          return Container(
+            margin: EdgeInsets.only(top: 10),
+            child: ListTile(
+              title: Text(result['FullName'],style: TextStyle(fontSize: fontSizeName,),maxLines: 1,overflow: TextOverflow.ellipsis,),
+              subtitle: Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 8),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Expanded(
+  child: SizedBox(
+    height: 40, // adjust the value as needed
+    child: Text(
+      '${result['Pairing']}',
+      style: TextStyle(fontSize: fontSizeDesc),
+      maxLines: 2,
+      overflow: TextOverflow.ellipsis,
+    ),
+  ),
+)
+                      ],
+                    ),
                   ),
+                  rsSectionWidth > 0 ? Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Text('RS ',style: TextStyle(fontSize: fontSizeSymbol)),
+                      Stack(
+                        children: [
+                          Container(
+                            width: rsSectionWidth,
+                            height: 20,
+                            decoration: BoxDecoration(
+                              color: Colors.grey.shade300,
+                            ),
+                          ),
+                          Positioned(
+                            top: 0,
+                            left: 0,
+                            child: Container(
+                              width: rsBarWidth,
+                              height: 20,
+                              decoration: BoxDecoration(
+                                color: Colors.blue,
+                              ),
+                            ),
+                          ),
+                          Positioned(
+                            top: 0,
+                            right: 0,
+                            child: Text(result['RS'].toInt().toString()),
+                          ),
+                        ],
+                      ),
+                      SizedBox(width: 6),
+                      Icon(Icons.leaderboard),
+                      Text(result['RANK'],style: TextStyle(fontSize: fontSizeSymbol)),
+                      Icon(Icons.star),
+                      Text(result['EvaluationAvg'],style: TextStyle(fontSize: fontSizeSymbol))
+                    ],
+                  ) : SizedBox(),
+                  SizedBox(height: 6),
+                  qpSectionWidth > 0 ? Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Text('QP ',style: TextStyle(fontSize: fontSizeSymbol,)),
+                      Stack(
+                        children: [
+                          Container(
+                            width: qpSectionWidth,
+                            height: 20,
+                            decoration: BoxDecoration(
+                              color: Colors.grey.shade300,
+                            ),
+                          ),
+                          Positioned(
+                            top: 0,
+                            left: 0,
+                            child: Container(
+                              width: qpBarWidth,
+                              height: 20,
+                              decoration: BoxDecoration(
+                                color: Colors.green,
+                              ),
+                            ),
+                          ),
+                          Positioned(
+                            top: 0,
+                            right: 0,
+                            child: Text(result['QP'].toInt().toString()),
+                          ),
+                        ],
+                      ),
+                      SizedBox(width: 6),
+                      Icon(Icons.euro),
+                      Text('${result['Price']}',style: TextStyle(fontSize: fontSizeSymbol,)),
+                    ],
+                  ) : SizedBox(),
+                ],
               ),
-              ],
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                Text('RS ',style: TextStyle(fontSize: fontSizeSymbol)),
-                Stack(
-                  children: [
-                    Container(
-                      width: 100,
-                      height: 20,
-                      decoration: BoxDecoration(
-                        color: Colors.grey.shade300,
-                        // boxShadow: [
-                        //   BoxShadow(
-                        //     color: Colors.black.withOpacity(0.5),
-                        //     spreadRadius: 1,
-                        //     blurRadius: 3,
-                        //     offset: Offset(0, 2),
-                        //   ),
-                        // ],
-                      ),
-                    ),
-                    Positioned(
-                      top: 0,
-                      left: 0,
-                      child: Container(
-                        width: result['RS'],
-                        height: 20,
-                        decoration: BoxDecoration(
-                          color: Colors.blue,
-                        ),
-                      ),
-                    ),
-                    Positioned(
-                      top: 0,
-                      right: 0,
-                      child: Text(result['RS'].toInt().toString()),
-                    ),
-                  ],
+              onTap: () {
+              final wineDetails = {
+                'fullName': result['FullName'],
+                'wineryName': result['WineryName'],
+                'appellationLevel': result['AppellationLevel'],
+                'appellationName': result['AppellationName'],
+                'region': result['Region'],
+                'grapes': result['Grapes'],
+                'pairing': result['Pairing'],
+                'agingMonths': result['AgingMonths'],
+                'agingType': result['AgingType'],
+                'rsScore': result['RS'],
+                'qpScore': result['QP'],
+                'rank': result['RANK'],
+                'price': result['Price'],
+              };
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => WineDetailsScreen(wineDetails),
                 ),
-                SizedBox(width: 6),
-                Icon(Icons.leaderboard),
-                Text(result['RANK'],style: TextStyle(fontSize: fontSizeSymbol)),
-                Icon(Icons.star),
-                Text(result['EvaluationAvg'],style: TextStyle(fontSize: fontSizeSymbol))
-              ],
+              );
+            },
             ),
-            SizedBox(height: 6),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                Text('QP ',style: TextStyle(fontSize: fontSizeSymbol,)),
-                Stack(
-                  children: [
-                    Container(
-                      width: 100,
-                      height: 20,
-                      decoration: BoxDecoration(
-                        color: Colors.grey.shade300,
-                      ),
-                    ),
-                    Positioned(
-                      top: 0,
-                      left: 0,
-                      child: Container(
-                        width: result['QP'],
-                        height: 20,
-                        decoration: BoxDecoration(
-                          color: Colors.green,
-                        ),
-                      ),
-                    ),
-                    Positioned(
-                      top: 0,
-                      right: 0,
-                      child: Text(result['QP'].toInt().toString()),
-                    ),
-                  ],
-                ),
-                SizedBox(width: 6),
-                Icon(Icons.euro),
-                Text('${result['Price']}',style: TextStyle(fontSize: fontSizeSymbol,)),
-              ],
-            ),
-          ],
-        ),
-        onTap: () {
-        final wineDetails = {
-          'fullName': result['FullName'],
-          'wineryName': result['WineryName'],
-          'appellationLevel': result['AppellationLevel'],
-          'appellationName': result['AppellationName'],
-          'region': result['Region'],
-          'grapes': result['Grapes'],
-          'pairing': result['Pairing'],
-          'agingMonths': result['AgingMonths'],
-          'agingType': result['AgingType'],
-          'rsScore': result['RS'],
-          'qpScore': result['QP'],
-          'rank': result['RANK'],
-          'price': result['Price'],
-        };
-        Navigator.push(
-          context,
-          MaterialPageRoute(
-            builder: (context) => WineDetailsScreen(wineDetails),
-          ),
-        );
-      },
-      )
+          );
+        },
+      ),
     );
   }
 }
--- lib/test_appellation.dart
+++ lib/test_appellation.dart
@@ -9,6 +9,7 @@ import 'navigation.dart';
 import 'search_sort_buttons.dart';
 import 'single_wine_tile.dart';
 
+
 Future<List<Map<String, dynamic>>> searchWines() async {
   var databasesPath = await getDatabasesPath();
   String path = join(databasesPath, 'allwines2.db');
@@ -92,6 +93,12 @@ class _EntriesScreenState extends State<EntriesScreen> {
   List<Map<String, dynamic>> entries = [];
   List<Map<String, dynamic>> groupedEntries = [];
   TextEditingController _searchController = TextEditingController();
+  Map<String, bool> _sortDescendingMap = {}; // add map to track sorting of each wine type
+
+  // Scroll Controller and List to enable Scroll to Group
+  final ScrollController _scrollController = ScrollController();
+  static const double _groupHeaderHeight = 32;
+  final List<String> _groupTitles = [];
 
   @override
   void initState() {
@@ -111,11 +118,22 @@ class _EntriesScreenState extends State<EntriesScreen> {
       String wineType = entry['WineType'];
       if (!wineTypeMap.containsKey(wineType)) {
         wineTypeMap[wineType] = [];
+        _sortDescendingMap[wineType] = true; // add wine type to the sort descending map
+        _groupTitles.add(wineType); // add wine type to the list of group titles
       }
       wineTypeMap[wineType]!.add(entry);
     });
     wineTypeMap.forEach((wineType, entries) {
       if (entries.isNotEmpty) {
+        // sort entries by RS value
+        entries.sort((a, b) {
+          bool sortDescending = _sortDescendingMap[wineType]!; // get sorting value from map
+          if (sortDescending) {
+            return b['RS'].compareTo(a['RS']);
+          } else {
+            return a['RS'].compareTo(b['RS']);
+          }
+        });
         groupedEntries.add({
           'WineType': wineType,
           'Entries': entries,
@@ -147,6 +165,20 @@ class _EntriesScreenState extends State<EntriesScreen> {
     return results;
   }
 
+  // Function to Scroll to Group
+  void _scrollToGroup(String groupTitle) {
+    int groupIndex = _groupTitles.indexOf(groupTitle);
+    double totalHeight = _groupHeaderHeight * groupIndex +
+        groupedEntries
+            .sublist(0, groupIndex)
+            .expand((group) => group['Entries'])
+            .length *
+            SingleWineTile.tileHeight;
+
+    _scrollController.animateTo(totalHeight,
+        duration: Duration(milliseconds: 500), curve: Curves.easeInOut);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -157,30 +189,76 @@ class _EntriesScreenState extends State<EntriesScreen> {
         children: [
           Expanded(
             child: ListView.builder(
+              controller: _scrollController, // add controller to access _scrollToGroup function
               itemCount: groupedEntries.length,
               itemBuilder: (context, index) {
                 final group = groupedEntries[index];
-                return group['Entries'].isNotEmpty
+                List<Map<String, dynamic>> entries = group['Entries']; // get wine type entries
+                bool sortDescending = _sortDescendingMap[group['WineType']]!; // get sorting value from map
+
+                // sort entries by RS value
+                entries.sort((a, b) {
+                  if (sortDescending) {
+                    return b['RS'].compareTo(a['RS']);
+                  } else {
+                    return a['RS'].compareTo(b['RS']);
+                  }
+                });
+
+                return entries.isNotEmpty
                     ? Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
-                          Padding(
-                            padding: const EdgeInsets.symmetric(
-                                horizontal: 16.0, vertical: 8.0),
-                            child: Text(
-                              group['WineType'],
-                              style: TextStyle(
-                                fontWeight: FontWeight.bold,
-                                fontSize: 16.0,
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Padding(
+                                padding: const EdgeInsets.only(
+                                    left: 16.0, top: 16),
+                                child: Text(
+                                  group['WineType'],
+                                  style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 20.0,
+                                  ),
+                                ),
                               ),
-                            ),
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.end,
+                                children: <Widget>[
+                                  Text(
+                                    'RS',
+                                    textAlign: TextAlign.center,
+                                    style: TextStyle(
+                                      fontSize: 16,
+                                      fontWeight: FontWeight.w500,
+                                    ),
+                                  ),
+                                  IconButton(
+                                    icon: Icon(Icons.arrow_downward),
+                                    onPressed: () {
+                                      setState(() {
+                                        _sortDescendingMap[group['WineType']] = false; // set sorting value to false
+                                      });
+                                    },
+                                  ),
+                                  IconButton(
+                                    icon: Icon(Icons.arrow_upward),
+                                    onPressed: () {
+                                      setState(() {
+                                        _sortDescendingMap[group['WineType']] = true; // set sorting value to true
+                                      });
+                                    },
+                                  ),
+                                ],
+                              ),
+                            ],
                           ),
                           ListView.builder(
                             shrinkWrap: true,
                             physics: ClampingScrollPhysics(),
-                            itemCount: group['Entries'].length,
+                            itemCount: entries.length,
                             itemBuilder: (context, index) {
-                              final entry = group['Entries'][index];
+                              final entry = entries[index];
                               return SingleWineTile(result: entry);
                             },
                           ),
@@ -190,9 +268,90 @@ class _EntriesScreenState extends State<EntriesScreen> {
               },
             ),
           ),
+          GroupingsWidget(
+            groupTitles: _groupTitles,
+            scrollToGroup: _scrollToGroup,
+          ),
         ],
       ),
       bottomNavigationBar: AGreatBottomNavigationBar(),
+    );
+  }
+}
+
+class GroupingsWidget extends StatelessWidget {
+  final List<String> groupTitles;
+  final Function scrollToGroup;
+
+  GroupingsWidget({
+    required this.groupTitles,
+    required this.scrollToGroup,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        showModalBottomSheet(
+          context: context,
+          builder: (context) {
+            return ListView.builder(
+              itemCount: groupTitles.length,
+              itemBuilder: (context, index) {
+                final item = groupTitles[index];
+                return ListTile(
+                  title: Text(item),
+                  onTap: () {
+                    Navigator.pop(context);
+                    scrollToGroup(item); // call to function to scroll to group
+                  },
+                );
+              },
+            );
+          },
+        );
+      },
+      child: Container(
+        height: 40,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(left: 16.0),
+              child: Text(
+                'Appellation wine types',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 16.0,
+                ),
+              ),
+            ),
+            IconButton(
+              icon: Icon(Icons.menu),
+              onPressed: () {
+                showModalBottomSheet(
+                  context: context,
+                  builder: (context) {
+                    return ListView.builder(
+                      itemCount: groupTitles.length,
+                      itemBuilder: (context, index) {
+                        final item = groupTitles[index];
+                        return ListTile(
+                          title: Text(item),
+                          onTap: () {
+                            Navigator.pop(context);
+                            scrollToGroup(item); // call to function to scroll to group
+                          },
+                        );
+                      },
+                    );
+                  },
+                );
+              },
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
