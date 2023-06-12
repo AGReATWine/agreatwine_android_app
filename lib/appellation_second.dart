33504f6aad7817f848bd44828678fcbec26633e9
d-cmst <dcmst@gmail.com>
2023-06-12 09:03:57 +0200

    various fixes

--- lib/appellation_second.dart
+++ lib/appellation_second.dart
@@ -19,7 +19,7 @@ String levelName = 'SLC';
 
 Future<List<Map<String, dynamic>>> searchWines() async {
   var databasesPath = await getDatabasesPath();
-  String path = join(databasesPath, 'allwines40.db');
+  String path = join(databasesPath, 'allwines41.db');
 
   Database database = await openDatabase(path);
   List<Map<String, dynamic>> results = await database.rawQuery(
